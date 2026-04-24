159c32b65e474ea8558aa5fa964114e744ed6f94
Savo Saicic <ssaicic@iliad-free.fr>
2026-04-24 11:34:04 +0200

    refactor(app0): remove sensor and GNSS features for minimal image

--- app0/include/gnss.h
+++
@@ -1,6 +0,0 @@
-#ifndef GNSS_H
-#define GNSS_H
-
-int gnss_init_and_start(void);
-
-#endif /* !GNSS_H */
--- app0/include/temperature.h
+++
@@ -1,6 +0,0 @@
-#ifndef TEMPERATURE_H
-#define TEMPERATURE_H
-
-int setup_temperature_sensor();
-
-#endif /* !TEMPERATURE_H */
